ae650cec07001440609d159f55aa50b906abb8da
aortiz <aortiz@hiberus.com>
2020-05-04 19:17:40 +0200

    Muevo la lógica a un nuevo proyecto

--- BattleArena.Bll/Arena.cs
+++ BattleArena.Bll/Arena.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using BattleArena.Entities.Player;
+using BattleArena.Entities;
 
-namespace BattleArena.Entities
+namespace BattleArena.Bll
 {
     public class Arena
     {
--- XUnitTestProject1/ArenaWithTwoPlatersShould.cs
+++ XUnitTestProject1/ArenaWithTwoPlatersShould.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using BattleArena.Bll;
 using BattleArena.Entities;
 using BattleArena.Entities.Player;
 using Xunit;
